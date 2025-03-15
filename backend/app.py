f21226f66acf828308a60b336a51790f77811b94
imesh-madushan <imeshmadushan927@gmail.com>
2025-03-16 02:12:05 +0530

    backend updated -check fraud and find suitable loan amount -enabled cors

--- backend/app.py
+++ backend/app.py
@@ -1,44 +1,37 @@
 import pandas as pd
 import pickle as pk
 import numpy as np
-import flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-# Model Predict loan approval
+app = Flask(__name__)
+CORS(app)
+app.config["DEBUG"] = True
+
+# load the models
 model_1 = pk.load(open('model_1.pkl','rb'))
-# scaler_1 = pk.load(open('scaler_1.pkl','rb'))
-
-# Model Loan Amount recommendation
 model_2 = pk.load(open('model_2.pkl','rb'))
-
-# Model Personalized Interest Rate (gives the cluster number)
 model_3 = pk.load(open('model_3.pkl','rb'))
-scaler_3 = pk.load(open('scaler_3.pkl','rb'))
-
-# Model Fraud Detection
 model_4 = pk.load(open('model_4.pkl','rb'))
+
+# load the scalers
+scaler_3 = pk.load(open('scaler_3.pkl','rb'))
 scaler_4 = pk.load(open('scaler_4.pkl','rb'))
 
 frud_threshold =  np.load('frud_threshold.npy')
 cluster_means = pd.DataFrame(pd.read_csv('cluster_means.csv'))
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
-@app.route('/')
-def welcome():
-    return 'Welcome All'
-
 @app.route('/check-approval',methods=["POST"])
 def predict_loan_approval():
     col_oder = ['no_of_dependents','education','self_employed','income_annum','loan_amount','loan_term','cibil_score','assets']
 
-    req = flask.request.get_json()
+    req = request.get_json()
     new_data = pd.DataFrame(req,index=[0])
 
     # check if all the required columns are provided
     for col in col_oder:
         if col not in new_data.columns:
-            return flask.jsonify({
+            return jsonify({
                 'error':'Please provide all the required columns',
                 'required_columns':col_oder
             }),400
@@ -49,54 +42,85 @@ def predict_loan_approval():
     if prediction[0] == 1:
         #check if requested loan is a fraud
         if check_fraud(new_data) == True:
-            return flask.jsonify({
+            return jsonify({
                 'approved': True,
                 'fraud': True
             })
         else:
-            return flask.jsonify({
+            return jsonify({
                 'approved': True,
                 'fraud': False
             })
     else:
-        return flask.jsonify({
+        return jsonify({
             'approved': False
         })
 
 @app.route('/recommend-loan',methods=["POST"])
 def recommend_loan_amount():
-    col_oder = ['income_annum', 'cibil_score', 'assets', 'loan_term', 'no_of_dependents', 'education', 'self_employed', 'loan_status']
+    col_oder = ['income_annum', 'cibil_score', 'assets', 'loan_term', 'no_of_dependents', 'education', 'self_employed']
 
-    req = flask.request.get_json()
+    req = request.get_json()
     new_data = pd.DataFrame(req,index=[0])
     
     # check if all the required columns are provided
     for col in col_oder:
         if col not in new_data.columns:
-            return flask.jsonify({
+            return jsonify({
                 'error':'Please provide all the required columns',
                 'required_columns':col_oder
             }),400
         
     new_data = new_data[col_oder] # reoder the columns as per the model trained
+
+    #add 'loan_status' to the col_oder with default value 1, to always get approved loan amount
+    new_data['loan_status'] = 1
     prediction = model_2.predict(new_data).astype(int)
 
-    #TODO: CHECK IF THE LOAN AMOUNT WILL BE FRAUD OR NOT
+    predicted_loan_amount = prediction[0]
+    data_with_pred_data = pd.DataFrame({
+        'income_annum': new_data['income_annum'],
+        'loan_amount': predicted_loan_amount,
+        'cibil_score': new_data['cibil_score'],
+        'assets': new_data['assets'],
+    })
 
-    return str(prediction[0])
+    if check_fraud(data_with_pred_data) == True:
+        #TODO: implement a better way to calculate the loan amount without fraud
+        new_loan_no_fraud = best_loan_no_fraud(data_with_pred_data)
+
+        if 'error' in new_loan_no_fraud:
+            return jsonify({
+                'error': str(new_loan_no_fraud['error'])
+                })
+        
+        elif 'warning' in new_loan_no_fraud:
+            return jsonify({
+                'loan_amount': str(new_loan_no_fraud['loan_amount']),
+                'warning': new_loan_no_fraud['warning']
+            })
+        else:
+            return jsonify({
+                'loan_amount': str(new_loan_no_fraud),
+            })
+    else:
+        return jsonify({
+            'loan_amount': str(predicted_loan_amount),
+            'fraud': False
+        })
 
 
 @app.route('/interest-rate',methods=["POST"])
 def cluster_number():
     col_oder = ["income_annum", "loan_amount"]
 
-    req = flask.request.get_json()
+    req = request.get_json()
     new_data = pd.DataFrame(req,index=[0])
 
     # check if all the required columns are provided
     for col in col_oder:
         if col not in new_data.columns:
-            return flask.jsonify({
+            return jsonify({
                 'error':'Please provide all the required columns',
                 'required_columns':col_oder
             }),400
@@ -121,7 +145,7 @@ def cluster_number():
         base_rate = 10
         interest_rate = base_rate + cluster_means['Interest Rate'][prediction[0]]  
 
-        return flask.jsonify({
+        return jsonify({
             'interest_rate': interest_rate,
             'fraud': True
         })
@@ -129,7 +153,7 @@ def cluster_number():
         base_rate = 10
         interest_rate = base_rate + cluster_means['Interest Rate'][prediction[0]]        
 
-        return flask.jsonify({
+        return jsonify({
             'interest_rate': interest_rate,
             'fraud': False
         })
@@ -137,15 +161,15 @@ def cluster_number():
 
 @app.route('/check-fraud',methods=["POST"])
 def fraud_detection():
-    req = flask.request.get_json()
+    req = request.get_json()
     new_data = pd.DataFrame(req,index=[0])
 
     if check_fraud(new_data) == True:
-        return flask.jsonify({
+        return jsonify({
             'fraud': True
         })
     else:
-        return flask.jsonify({
+        return jsonify({
             'fraud': False
         })
 
@@ -155,7 +179,7 @@ def check_fraud(data):
 
     for col in col_oder:
         if col not in data.columns:
-            return flask.jsonify({
+            return jsonify({
                 'error':'Please provide all the required columns',
                 'required_columns':col_oder
             }),400
@@ -167,6 +191,52 @@ def check_fraud(data):
     test_distance = np.linalg.norm(new_data_scaled - model_4.cluster_centers_[prediction_clustor])
     test_fraud = test_distance > frud_threshold
     return test_fraud
+
+# when fruaud is detected, find the best possible loan amount
+def best_loan_no_fraud(data):
+    col_order = ["income_annum", "loan_amount", "cibil_score", "assets"]  # This contains the fraud loan amount
+    reduce_limit_hit = False
+
+    for col in col_order:
+        if col not in data.columns:
+            return jsonify({
+                'error': 'Please provide all the required columns',
+                'required_columns': col_order
+            }), 400
+
+    new_data = data[col_order]
+
+    new_data_scaled = scaler_4.transform(new_data)
+    prediction_cluster = model_4.predict(new_data_scaled)
+    test_distance = np.linalg.norm(new_data_scaled - model_4.cluster_centers_[prediction_cluster])
+
+    # if loan amount is causing fraud, reduce it step by step
+    if test_distance > frud_threshold:
+        step = 0.95  
+        while test_distance > frud_threshold and new_data["loan_amount"].values[0] > 1000:  # Ensure loan amount doesn't go too low
+            new_data["loan_amount"] *= step  # Reduce loan amount
+            print(new_data["loan_amount"].values[0])
+            new_data_scaled = scaler_4.transform(new_data)
+            prediction_cluster = model_4.predict(new_data_scaled)
+            test_distance = np.linalg.norm(new_data_scaled - model_4.cluster_centers_[prediction_cluster])
+
+            #stop reducing if new loan amount is 70% of the original loan amount
+            if new_data["loan_amount"].values[0] < 0.70 * data["loan_amount"].values[0]:
+                reduce_limit_hit = True
+                break
+
+        if reduce_limit_hit:
+            return {
+                'error': 'Loan amount cannot be calculate without causing fraud'
+            }
+        else:
+            return {
+                'loan_amount': int(new_data["loan_amount"].values[0]),
+                'warning': 'Actual predicted Loan amount reduced to avoid fraud'
+            }
+
+    return int(new_data["loan_amount"].values[0])
+
 
 if __name__ == '__main__':
     app.run(port=5000)
